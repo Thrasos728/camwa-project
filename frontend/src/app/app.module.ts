b08fec9fe5f3ccac6a44646993f3ab19889f182f
Thrasos <tnta728@gmail.com>
2024-11-13 18:53:49 +0700

    update node version

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireAnalyticsModule } from "@angular/fire/compat/analytics";
 import { environment } from "../environments/environment";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
 
